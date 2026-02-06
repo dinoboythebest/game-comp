9f2d360c25760561f4a2722ad89767cd20d5bdec
dinoboythebest <dinoboy2011ta@gmail.com>
2026-02-05 20:26:48 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+print("hello world")
+print("hello player 1")
+print("welcome to the world of python")
+a=11
+b=11.1
+c="my name"
+d=a + b
+print (f'{d} is {a}{b}')
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
